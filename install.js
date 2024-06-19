e46dcd8086c0747cc483a842013f743697ba00ad
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-18 20:45:36 -0400

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/peanutcocktail/hallo app",
+          "git clone https://github.com/cocktailpeanut/hallo app",
         ]
       }
     },
